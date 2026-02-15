cdc84c45970529627a233de3f2505cb3eb2df999
Hilmar Wiegand <me@hwgnd.de>
2026-02-15 23:44:36 +0100

    global

--- src/wayland/mod.rs
+++ src/wayland/mod.rs
@@ -10,7 +10,7 @@ use calloop::{
   channel::{Channel, Event},
 };
 use calloop_wayland_source::WaylandSource;
-use gpui::App;
+use gpui::{App, Entity, EventEmitter, Global, prelude::*};
 use tracing::{debug, error, info};
 use wayland_client::{
   Connection, Dispatch, QueueHandle, delegate_noop, event_created_child,
@@ -24,21 +24,43 @@ use wayland_protocols_wlr::data_control::v1::client::{
 #[derive(Debug)]
 pub enum Command {}
 
+#[derive(Debug, Clone)]
+pub enum WaylandEvent {}
+
+struct GlobalWaylandConnection(Entity<WaylandConnection>);
+
+impl Global for GlobalWaylandConnection {}
+
+pub struct WaylandConnection {
+  cmd_tx: calloop::channel::Sender<Command>,
+  signal: LoopSignal,
+}
+
+impl EventEmitter<WaylandEvent> for WaylandConnection {}
+
+impl WaylandConnection {
+  pub fn global(cx: &App) -> Entity<Self> {
+    cx.global::<GlobalWaylandConnection>().0.clone()
+  }
+}
+
 #[derive(Default)]
 struct State {
   seat: Option<wl_seat::WlSeat>,
   data_device: Option<zwlr_data_control_device_v1::ZwlrDataControlDeviceV1>,
   data_manager: Option<zwlr_data_control_manager_v1::ZwlrDataControlManagerV1>,
+  event_tx: Option<flume::Sender<WaylandEvent>>,
 }
 
 pub fn init(cx: &mut App) -> Result<()> {
   let (cmd_tx, cmd_rx) = calloop::channel::channel::<Command>();
   let (init_tx, init_rx) = flume::bounded::<LoopSignal>(1);
+  let (event_tx, event_rx) = flume::unbounded::<WaylandEvent>();
 
   // Bit awkward since we can't move the event loop into the background thread, so we have to send
   // the signal back to the main thread after init.
   thread::spawn(move || {
-    if let Err(err) = run(cmd_rx, init_tx) {
+    if let Err(err) = run(cmd_rx, init_tx, event_tx) {
       error!(?err, "Error in wayland connection");
     }
   });
@@ -46,16 +68,47 @@ pub fn init(cx: &mut App) -> Result<()> {
   // If an error occurs during startup, this fails and the main thread exits as well.
   let signal = init_rx.recv()?;
 
+  let connection = cx.new(|_| WaylandConnection { cmd_tx, signal });
+  cx.set_global(GlobalWaylandConnection(connection.clone()));
+
+  cx.on_app_quit({
+    let connection = connection.clone();
+    move |cx| {
+      connection.read(cx).signal.stop();
+      async {}
+    }
+  })
+  .detach();
+
+  cx.spawn({
+    let connection = connection.clone();
+    async move |cx| {
+      while let Ok(event) = event_rx.recv_async().await {
+        connection.update(cx, |_, cx| {
+          cx.emit(event);
+        });
+      }
+    }
+  })
+  .detach();
+
   Ok(())
 }
 
-fn run(cmd_rx: Channel<Command>, init_tx: flume::Sender<LoopSignal>) -> Result<()> {
+fn run(
+  cmd_rx: Channel<Command>,
+  init_tx: flume::Sender<LoopSignal>,
+  event_tx: flume::Sender<WaylandEvent>,
+) -> Result<()> {
   let conn = Connection::connect_to_env()?;
   let display = conn.display();
   let mut event_queue = conn.new_event_queue();
   let qh = event_queue.handle();
   let _registry = display.get_registry(&qh, ());
-  let mut state = State::default();
+  let mut state = State {
+    event_tx: Some(event_tx),
+    ..Default::default()
+  };
 
   event_queue.roundtrip(&mut state)?;
 
